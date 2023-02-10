99290fa5fad401de16d8302aefed6f82ed2c8a71
gias-uddin-swe <nirobh753@gmail.com>
2023-02-10 15:04:50 +0600

    tesitng conflict

--- gias.js
+++ gias.js
@@ -1,4 +1,7 @@
 const friend = ["rahim", "korim","giassss","perosn22222", "hero alom", "kiara advani", "pitter"];
 
 const result = friend.find((name) => name === "rahim");
+const result = friend.find((name) => name === "korim");
+
+
 // console.log(result);
