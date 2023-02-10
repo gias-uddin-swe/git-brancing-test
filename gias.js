c009cf2b2ca609a5ab6f71a0af98fdb6e68e619d
gias-uddin-swe <nirobh753@gmail.com>
2023-02-10 15:05:41 +0600

    check form gais/bashbaord branc

--- gias.js
+++ gias.js
@@ -1,4 +1,4 @@
 const friend = ["rahim", "korim", "hero alom", "kiara advani", "pitter"];
 
-const result = friend.find((name) => name === "rahim");
-// console.log(result);
+const result = friend.find((name) => name === "pitter");
+console.log(result);
